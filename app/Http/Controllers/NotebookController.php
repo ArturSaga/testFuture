69eea40761088a72aa5ca25a021f8213372d1cbe
mac <asagataev@list.ru>
2022-02-16 02:25:57 +0700

    Fix NotebookController

--- app/Http/Controllers/NotebookController.php
+++ app/Http/Controllers/NotebookController.php
@@ -3,18 +3,28 @@
 namespace App\Http\Controllers;
 
 use App\Models\Notebook;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Pagination\LengthAwarePaginator;
+
 
 class NotebookController extends Controller
 {
-
-    public function index()
+    /**
+     * @return LengthAwarePaginator
+     */
+    public function index(): LengthAwarePaginator
     {
         $notebook = Notebook::paginate(5);
+
         return $notebook;
     }
 
-    public function store(Request $request)
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function store(Request $request): JsonResponse
     {
         $request->validate([
             'name'=>['required','string','min:3'],
@@ -22,38 +32,51 @@ class NotebookController extends Controller
             'email'=>['required','string','min:3']
         ]);
         $notebook = Notebook::create($request->all());
+
         return response()->json($notebook, 201);
     }
 
-    public function show($id)
+    /**
+     * @param int $id
+     * @return JsonResponse
+     */
+    public function show(int $id): JsonResponse
     {
         $notebook = Notebook::find($id);
-        if (is_null($notebook))
-        {
+        if (is_null($notebook)) {
             return response()->json(['error'=> true, 'message'=>'Not found'],404);
         }
+
         return response()->json($notebook,200);
     }
 
-    public function update(Request $request, $id)
+    /**
+     * @param Request $request
+     * @param int $id
+     * @return JsonResponse
+     */
+    public function update(Request $request, int $id): JsonResponse
     {
         $notebook = Notebook::find($id)->update($request->all());
-        if (is_null($notebook))
-        {
+        if (is_null($notebook)) {
             return response()->json(['error'=> true, 'message'=>'Not found'],404);
         }
 
         return response()->json($notebook, 200);
     }
 
-
-    public function destroy(Request $request, $id)
+    /**
+     * @param Request $request
+     * @param int $id
+     * @return JsonResponse
+     */
+    public function destroy(Request $request, int $id): JsonResponse
     {
         $notebook = Notebook::find($id)->delete($request->all());
-        if (is_null($notebook))
-        {
-            return response()->json(['error'=> true, 'message'=>'Not found'],404);
+        if (is_null($notebook)) {
+            return response()->json(['error'=> true, 'message'=>'Not found '],404);
         }
+
         return response()->json($notebook, 204);
     }
 }
